575f23f8950333b5cc7eea16cb9d040f0af13a50
anonimous-user <harrisbond1234@gmail.com>
2025-04-02 18:32:22 -0400

    new hardware tested. ASM file modified to contain example usages

--- NewLEDsTest.asm
+++ NewLEDsTest.asm
@@ -1,8 +1,26 @@
 ORG 0
 
-loop:
-loadi &b111000111
+loadi &b0000000001 ;choose right most LED
 out PWM
+loadi &h64 ;100%
+out PWM
+
+loadi &b0000111000 ;choose 3 LEDs 3 from right
+out PWM
+loadi &h32 ;50%
+out PWM
+
+loadi &b1100000000 ;choose left 2 most LED
+out PWM
+load reset_Ctrl_state ;reset state to LED select
+out PWM
+loadi &b0010000000 ;choose 3rd from left LED
+out PWM
+loadi &h16 ;25%
+out PWM
+
+loop:
 jump loop
 
 PWM: 	EQU 032
+reset_Ctrl_state:	DW &b1000000000000000
