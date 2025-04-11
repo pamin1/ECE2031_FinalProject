60d2b3618e847ddaad4064d8f24082b11d89ef16
s-sakharkar <138175130+s-sakharkar@users.noreply.github.com>
2025-04-10 21:59:08 -0700

    Update NewLEDsTest.asm
    Waits until a switch is raised. Once a switch is raised, the corresponding LED increases in brightness by 12.5% of max brightness each time. The program then waits until all switches are lowered to start again.

--- NewLEDsTest.asm
+++ NewLEDsTest.asm
@@ -1,26 +1,133 @@
 ORG 0
 
-loadi &b0000000001 ;choose right most LED
-out PWM
-loadi &h64 ;100%
-out PWM
-
-loadi &b0000111000 ;choose 3 LEDs 3 from right
-out PWM
-loadi &h32 ;50%
-out PWM
-
-loadi &b1100000000 ;choose left 2 most LED
-out PWM
-load reset_Ctrl_state ;reset state to LED select
-out PWM
-loadi &b0010000000 ;choose 3rd from left LED
-out PWM
-loadi &h16 ;25%
-out PWM
-
 loop:
-jump loop
+in switches
+jzero loop
 
-PWM: 	EQU 032
+Next:
+out PWM
+
+go:
+shift -1
+jzero inc0
+shift -1
+jzero inc1
+shift -1
+jzero inc2
+shift -1
+jzero inc3
+shift -1
+jzero inc4
+shift -1
+jzero inc5
+shift -1
+jzero inc6
+shift -1
+jzero inc7
+shift -1
+jzero inc8
+shift -1
+jzero inc9
+
+error:
+jump error
+
+inc0:
+load b0
+addi &h8 ; currently increases brightness by 12.5%
+store b0
+out PWM
+jump wait
+
+inc1:
+load b1
+addi &h8
+store b1
+out PWM
+jump wait
+
+inc2:
+load b2
+addi &h8
+store b2
+out PWM
+jump wait
+
+inc3:
+load b3
+addi &h8
+store b3
+out PWM
+jump wait
+
+inc4:
+load b4
+addi &h8
+store b4
+out PWM
+jump wait
+
+inc5:
+load b5
+addi &h8
+store b5
+out PWM
+jump wait
+
+inc6:
+load b6
+addi &h8
+store b6
+out PWM
+jump wait
+
+inc7:
+load b7
+addi &h8
+store b7
+out PWM
+jump wait
+
+inc8:
+load b8
+addi &h8
+store b8
+out PWM
+jump wait
+
+inc9:
+load b9
+addi &h8
+store b9
+out PWM
+jump wait
+
+wait:
+in switches
+jzero loop
+jump wait
+
+
+end:
+jump end
+
+; variables for brightness levels
+b0: 		DW 0
+b1: 		DW 0
+b2: 		DW 0
+b3: 		DW 0
+b4: 		DW 0
+b5: 		DW 0
+b6: 		DW 0
+b7: 		DW 0
+b8: 		DW 0
+b9: 		DW 0
+
+
+Switches:  	EQU 000
+LEDs:      	EQU 001
+Timer:     	EQU 002
+Hex0:      	EQU 004
+Hex1:      	EQU 005
+PWM:		EQU 032
 reset_Ctrl_state:	DW &b1000000000000000
